7dd6a1c0a1aecc9c2bef3007e7e2080cbb68fd80
oysteinwangen <oysteinwangen95@gmail.com>
2021-06-06 19:51:24 +0200

    Added success message to form

--- js/contact.js
+++ js/contact.js
@@ -5,26 +5,35 @@ const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
 const message = document.querySelector("#message");
 const messageError = document.querySelector("#messageError");
+const successMessage = document.querySelector(".successmessage");
 
 function validateForm(event) {
   event.preventDefault();
-
+  let numberOfCorrect = "";
   if (checkLength(fullName.value, 0)) {
     fullNameError.style.display = "none";
+    numberOfCorrect += "1";
   } else {
     fullNameError.style.display = "block";
   }
 
   if (validateEmail(email.value)) {
     emailError.style.display = "none";
+    numberOfCorrect += "1";
   } else {
     emailError.style.display = "block";
   }
 
   if (checkLength(message.value, 24)) {
     messageError.style.display = "none";
+    numberOfCorrect += "1";
   } else {
     messageError.style.display = "block";
+  }
+
+  if (numberOfCorrect === "111") {
+    successMessage.style.display = "block";
+    form.style.display = "none";
   }
 }
 
